956d5c188438e30c25d91d58b900611b0a7bffa3
Sachin Tewari <steelrooter@users.noreply.github.com>
2018-11-26 17:43:59 +0800

    Add support for SpannableString (#20)
    * Upgrade dependencies
    
    * Add support for SpannableString
    
    * Bump version to 1.5.0
    
    * Simplify Parcel CREATOR
    
    * Add an example usage for ObservableSpannableString
    
    * Use apply for the example SpannableString

---
+++ lib/src/main/java/com/stepango/rxdatabindings/ObservableSpannableString.kt
@@ -0,0 +1,37 @@
+package com.stepango.rxdatabindings
+
+import android.databinding.ObservableField
+import android.os.Parcel
+import android.os.Parcelable
+import android.text.SpannableString
+import android.text.TextUtils
+
+
+/**
+ * Observable wrapper for non-nullable SpannableString that implements Parcelable contract
+ */
+class ObservableSpannableString(
+        value: SpannableString = SpannableString("")
+) :
+        ObservableField<SpannableString>(value),
+        Parcelable {
+
+    override fun describeContents(): Int = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) = TextUtils.writeToParcel(get(), dest, flags)
+
+    override fun get(): SpannableString = super.get() ?: throw IllegalStateException()
+
+    @Suppress("RedundantOverride")
+    override fun set(value: SpannableString) = super.set(value)
+
+    companion object CREATOR : Parcelable.Creator<ObservableSpannableString> {
+
+        override fun createFromParcel(source: Parcel) = ObservableSpannableString(
+                TextUtils.CHAR_SEQUENCE_CREATOR.createFromParcel(source) as SpannableString
+        )
+
+        override fun newArray(size: Int): Array<ObservableSpannableString?> = arrayOfNulls(size)
+    }
+
+}
